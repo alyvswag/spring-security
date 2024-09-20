436dd4921c73eca4c9dbb06b2f02b040f53a3f05
Talib Aliyev <133005678+alyvswag@users.noreply.github.com>
2024-09-20 19:42:26 +0400

    generate public and private key methods

--- src/main/java/com/example/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/example/springsecurity/SpringSecurityApplication.java
@@ -12,6 +12,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Base64;
+
 @SpringBootApplication
 public class SpringSecurityApplication implements CommandLineRunner {
 
@@ -19,22 +25,56 @@ public class SpringSecurityApplication implements CommandLineRunner {
         SpringApplication.run(SpringSecurityApplication.class, args);
     }
 
-    @Value("${authentication.jwt.access-expiration-in-ms}")
-    private Long JWT_ACCESS_EXPIRATION_IN_MS;
-    @Value("${authentication.jwt.refresh-expiration-in-ms}")
-    private Long JWT_REFRESH_EXPIRATION_IN_MS;
+//    @Value("${authentication.jwt.access-expiration-in-ms}")
+//    private Long JWT_ACCESS_EXPIRATION_IN_MS;
+//    @Value("${authentication.jwt.refresh-expiration-in-ms}")
+//    private Long JWT_REFRESH_EXPIRATION_IN_MS;
+//
+//    @Autowired
+//    private JwtProvider jwtProvider;
+//    @Autowired
+//    private UserRepository userRepository;
 
-    @Autowired
-    private JwtProvider jwtProvider;
-    @Autowired
-    private UserRepository userRepository;
-
+//    @Override
+//    public void run(String... args) throws Exception {
+//        User user = userRepository.findByUserId(1);
+//        String accessToken = jwtProvider.generateToken(user, JWT_ACCESS_EXPIRATION_IN_MS);
+//        System.out.println(accessToken);
+//        String subject = jwtProvider.getSubject(accessToken);
+//        System.out.println(subject);
+//    }
     @Override
     public void run(String... args) throws Exception {
-        User user = userRepository.findByUserId(1);
-        String accessToken = jwtProvider.generateToken(user, JWT_ACCESS_EXPIRATION_IN_MS);
-        System.out.println(accessToken);
-        String subject = jwtProvider.getSubject(accessToken);
-        System.out.println(subject);
+        KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("RSA");
+        keyGenerator.initialize(1024);
+        KeyPair kp = keyGenerator.genKeyPair();
+        PublicKey publicKey = kp.getPublic();
+        PrivateKey privateKey = kp.getPrivate();
+
+        String encodedPublicKey = Base64.getEncoder().encodeToString(publicKey.getEncoded());
+        String encodedPrivateKey = Base64.getEncoder().encodeToString(privateKey.getEncoded());
+
+        System.out.println(convertToPublicKey(encodedPublicKey));
+
+        System.out.println();
+
+        System.out.println(convertToPrivateKey(encodedPrivateKey));
+
+    }
+
+    private static String convertToPrivateKey(String key) {
+        StringBuilder result = new StringBuilder();
+        result.append("-----BEGIN PRIVATE KEY-----\n");
+        result.append(key);
+        result.append("\n-----END PRIVATE KEY-----");
+        return result.toString();
+    }
+
+    private static String convertToPublicKey(String key) {
+        StringBuilder result = new StringBuilder();
+        result.append("-----BEGIN PUBLIC KEY-----\n");
+        result.append(key);
+        result.append("\n-----END PUBLIC KEY-----");
+        return result.toString();
     }
 }
